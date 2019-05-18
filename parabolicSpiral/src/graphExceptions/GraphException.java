e6fae2dd750ee86b6a652f9a804fa20611cc369e
Vadym Nakytniak <vadim.nakitnyak@gmail.com>
2019-05-18 20:34:35 +0300

    Exceptions added

--- parabolicSpiral/src/graphExceptions/GraphException.java
+++ parabolicSpiral/src/graphExceptions/GraphException.java
@@ -5,10 +5,87 @@
  */
 package graphExceptions;
 
+import java.io.PrintStream;
+import java.io.PrintWriter;
+
 /**
  *
  * @author VADIM
  */
-public class GraphException {
+public class GraphException extends Exception{
+
+    public GraphException() {
+    }
+
+    public GraphException(String message) {
+        super(message);
+    }
+
+    public GraphException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public GraphException(Throwable cause) {
+        super(cause);
+    }
+
+    public GraphException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+
+    @Override
+    public void setStackTrace(StackTraceElement[] stackTrace) {
+        super.setStackTrace(stackTrace); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public StackTraceElement[] getStackTrace() {
+        return super.getStackTrace(); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public synchronized Throwable fillInStackTrace() {
+        return super.fillInStackTrace(); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void printStackTrace(PrintWriter s) {
+        super.printStackTrace(s); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void printStackTrace(PrintStream s) {
+        super.printStackTrace(s); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void printStackTrace() {
+        super.printStackTrace(); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String toString() {
+        return super.toString(); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public synchronized Throwable initCause(Throwable cause) {
+        return super.initCause(cause); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public synchronized Throwable getCause() {
+        return super.getCause(); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String getLocalizedMessage() {
+        return super.getLocalizedMessage(); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String getMessage() {
+        return super.getMessage(); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }
